269089f18bcf8c4798c9d9f8d9af87cb17820db7
suejungshin <suejung@ketch.com>
2024-06-19 09:47:06 -0700

    add values

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Middleware to manipulate incoming requests
 app.use(express.json());
 
 // Route handling
-app.post('*', (req, res) => {
+app.post('/route2', (req, res) => {
+  console.log('hit route 2')
+  console.log(req.headers)
+  console.log(JSON.stringify(req.body))
+
   // Creating a custom JSON response
   const jsonResponse = {
     "apiVersion": "dsr/v1",
@@ -17,13 +21,73 @@ app.post('*', (req, res) => {
     },
     "response": {
       "status": "completed",
-      // "identities": [
-      //   {
-      //     "identitySpace": "_mkto_trk",
-      //     "identityFormat": "raw",
-      //     "identityValue": "idHere"
-      //   }
-      // ],
+      "resultMessage": "some result",
+      "identities": [
+        {
+          "identitySpace": "_mkto_trk",
+          "identityFormat": "raw",
+          "identityValue": "idHere"
+        }
+        // {
+        //   "identitySpace": "not_exists",
+        //   "identityFormat": "raw",
+        //   "identityValue": "This identity space does not exist"
+        // }
+      ],
+      "subject": {
+        "type": "customer",
+        "email": "test@test.com",
+        "firstName": "Test",
+        "lastName": "Test",
+        "addressLine1": "1 Liverpool Road",
+        "city": "Liverpool",
+        "postalCode": "L1 1AA",
+        "countryCode": "GB"
+      },
+      "documents": [
+        {
+          "url": "https://blog.leahhanson.us/images/HVIF/FileSizes.png",
+          "headers": {
+            "Authorization": "auth"
+          }
+        }
+      ],
+    }
+  }
+
+
+  // setTimeout(()=>{
+  //   console.log("timed out")
+// res.status(200).json(jsonResponse);
+  // }, 60*1000)
+
+
+  // Sending the JSON response
+  res.status(201).json(jsonResponse);
+
+  // res.status(404)
+});
+
+app.post('/route1', (req, res) => {
+  console.log('hit route 1')
+  // Creating a custom JSON response
+  const jsonResponse = {
+    "apiVersion": "dsr/v1",
+    "kind": "AccessResponse",
+    "metadata": {
+      "uid": req.body.metadata.uid,
+      "tenant": req.body.metadata.tenant
+    },
+    "response": {
+      "status": "cancelled",
+      "resultMessage": "some result",
+      "identities": [
+        {
+          "identitySpace": "_mkto_trk",
+          "identityFormat": "raw",
+          "identityValue": "idHere"
+        }
+      ],
       "subject": {
         "type": "customer",
         "email": "test@test.com",
@@ -38,8 +102,9 @@ app.post('*', (req, res) => {
   }
 
   // Sending the JSON response
-  res.json(jsonResponse);
+  // res.status(200).json(jsonResponse);
 });
+
 
 // Starting the server
 app.listen(port, () => {
